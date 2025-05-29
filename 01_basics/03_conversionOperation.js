1f75c9e36c4e53a268feee1e409ef42a1a699bc3
mohit mukati <mohitmukatii49@gmail.com>
2025-05-29 10:15:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "null"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "45" => 45
+// "45abc" => NaN
+// true => 1; false =0
+
+let isLoggedIn = "mohit"
+
+let boleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(boleanisLoggedIn);
+
+// 1 = true; 0 = false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
