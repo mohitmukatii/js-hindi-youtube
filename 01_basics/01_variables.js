5dee682f6bdbf140a2a9479359c757e2124d4e52
mohit mukati <mohitmukatii49@gmail.com>
2025-05-29 09:14:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "mohit@email.com"
+var accountPassword = 4567
+accountCity = "indore" 
+
+//accountEmail=8758
+
+
+accountEmail = "mm@mm.com"
+accountPassword = "2345"
+accountCity = "bengluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+/* 
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
