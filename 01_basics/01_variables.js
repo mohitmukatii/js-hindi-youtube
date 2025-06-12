fa67371612310dbaf7058052fa69a383cda681ee
mohit mukati <mohitmukatii49@gmail.com>
2025-06-12 13:20:47 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 14453;
+let accountEmail = "mohit@gmail.com";
+var accountPassword = 8946
+accountCity =  "indore"
+let accountState ;
+
+
+
+accountEmail = "mm@mm.com";
+accountPassword = 93933;
+accountCity = "bengluru"
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ]);
+
+
